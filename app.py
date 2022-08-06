53e66b3ac6e31b0d54413bcfda3f565b3a431a78
Judah George <georgejudah5@gmail.com>
2022-08-06 15:09:49 +0530

    updated the conditions for single or multiple urls

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import requests
 urls = ['https://www.google.com/', 'https://www.facebook.com/']
 
 def check_connection(urls):
-    for each in urls:    
+    if len(urls) > 1:
+        for each in urls:    
+            data = requests.get(each)
+            print(data.status_code)
+    else:
         data = requests.get(each)
         print(data.status_code)
 
