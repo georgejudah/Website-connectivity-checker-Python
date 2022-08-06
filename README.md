dbc5784662a80fbebdac3a59dacbdf1dabeeecc4
Judah George <georgejudah5@gmail.com>
2022-08-06 15:08:37 +0530

    now using requests package& d:/work/python_projects/site_connectivity_checker/venv/Scripts/python.exe d:/work/python_projects/site_connectivity_checker/app.py

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
-PRODUCT NAME
+# Site Connectivity checker
 
-This application is used to check whether one/more websites are live or not using its status code
+***This application is used to check whether one/more websites are live or not using its status code***
 
-INSTALLATION/RUN
-python app.py
+## Author
+* Email ID - [email](georgejudah5@gmail.com)
+* Nanme - **Judah George**
 
-USAGE
+
+## INSTALLATION/RUN
+
+* *This application requires requests module*
+
+```
+pip install requests
+```
+
+* *Use `python app.py` to run the application*
+
+## USAGE
 Add the list of urls that you want to check for connectivity inside the variable called urls just like I have done
-for added benifit, you could run this app on a schedule using the windows task scheduler and could also use python logger to store all details on each run
-in a logfile
-
-META
-Judah - mail - georgejudah5@gmail.com
+and then run the application
--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 import http.client
+import requests
+
+urls = ['https://www.google.com/', 'https://www.facebook.com/']
 
 def check_connection(urls):
-    try:
-        for url in urls:
-            connection =  http.client.HTTPConnection(f"{url}")
-            connection.request('GET', '/')
-            response = connection.getresponse()
-            print(f"App: {url}, Status: {response.status} and reason: {response.reason}")
-            if response.status == 200:
-                print(f"Successfull response code")
-            else:
-                print(f"App:{url} looks down with status: {response.status} and reason: {response.reason}, please check the application")
-    except Exception as e:
-        print("an error occured")
-    connection.close()
-
-urls = ["www.google.com", "www.facebook.com", "www.twitter.com"]
+    for each in urls:    
+        data = requests.get(each)
+        print(data.status_code)
 
 check_connection(urls)
 
